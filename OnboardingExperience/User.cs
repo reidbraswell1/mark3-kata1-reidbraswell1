8f94d9dde348181457424469facd1d3a6f223e6a
reid <reidbras@gmail.com>
2018-04-11 11:10:37 -0500

    Modified IsAccountOwner from private to public

--- OnboardingExperience/User.cs
+++ OnboardingExperience/User.cs
@@ -7,7 +7,7 @@ public class User {
     public string LastName { get; set; }
     public string FirstName { get; set; }
 
-    private bool IsAccountOwner { get; set; };
+    public bool IsAccountOwner { get; set; }
 
     public byte PinNumber { get; set; }
 
