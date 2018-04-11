86b24aa36099903bd9d2ed8eed3946c42a527025
reid <reidbras@gmail.com>
2018-04-11 11:55:02 -0500

    Adding extra fields Address,City,State and ZipCode to User.cs

--- OnboardingExperience/User.cs
+++ OnboardingExperience/User.cs
@@ -6,9 +6,11 @@ public class User {
 
     public string LastName { get; set; }
     public string FirstName { get; set; }
-
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public short ZipCode { get; set; }
     public bool IsAccountOwner { get; set; }
-
     public byte PinNumber { get; set; }
 
     // Default Constructor
