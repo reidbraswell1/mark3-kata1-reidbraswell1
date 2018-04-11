b37373dbadb631a3aa68ef1eb61e6be8a0257cd6
reid <reidbras@gmail.com>
2018-04-11 13:48:42 -0500

    Changed ZipCode from short to int and PinNumber form byte to int

--- OnboardingExperience/User.cs
+++ OnboardingExperience/User.cs
@@ -9,9 +9,9 @@ public class User {
     public string Address { get; set; }
     public string City { get; set; }
     public string State { get; set; }
-    public short ZipCode { get; set; }
+    public int ZipCode { get; set; }
     public bool IsAccountOwner { get; set; }
-    public byte PinNumber { get; set; }
+    public int PINNumber { get; set; }
 
     // Default Constructor
     public User() 
