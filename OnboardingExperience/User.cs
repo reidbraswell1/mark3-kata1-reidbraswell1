62b9036e4fb296987a49eb05a74c10799439ab68
reid <reidbras@gmail.com>
2018-04-12 10:54:57 -0500

    Adding Namespace

--- OnboardingExperience/User.cs
+++ OnboardingExperience/User.cs
@@ -1,21 +1,24 @@
-/*
- *  User Class that defines 
- *  the user.
- */
-public class User {
+namespace OnboardingExperience
+{
+    /*
+     *  User Class that defines 
+     *  the user.
+     */
+    public class User {
 
-    public string LastName { get; set; }
-    public string FirstName { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public int ZipCode { get; set; }
-    public bool IsAccountOwner { get; set; }
-    public int PINNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public bool IsAccountOwner { get; set; }
+        public int PINNumber { get; set; }
 
-    // Default Constructor
-    public User() 
-    {
+        // Default Constructor
+        public User() 
+         {
 
+        }//UserConstructor//
     }//User//
-}//User//
+}//OnboardingExperience//
