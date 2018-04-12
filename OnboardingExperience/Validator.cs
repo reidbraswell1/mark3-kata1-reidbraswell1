f8293cf9d7839ea12ce3d94ebface3b9cf84984e
reid <reidbras@gmail.com>
2018-04-12 09:36:46 -0500

    Adding comments

--- OnboardingExperience/Validator.cs
+++ OnboardingExperience/Validator.cs
@@ -1,10 +1,17 @@
 using System;
+/*
+ *
+ *  Static Class helper to assist with console input/output and validation.
+ */
 static class Validator
 {
         private const int PIN_NUMBER_LENGTH = 4;
         private const int ZIP_CODE_LENGTH = 5;
         private const int MIN_RESPONSE_LENGTH = 2;
 
+        /*
+         * Ask for and validate the pin code.
+         */
         public static int AskQuestionPINCode(string question)
         {
             AskQuestion(question);
@@ -20,6 +27,10 @@ static class Validator
                 DisplayErrorText("PIN Number must be 4 numeric digits please try again.");
             }//while//
         }//AskQuestionPINCode//
+
+        /*
+         * Ask for and validate a general string field..
+         */
         public static string AskQuestionString(string question) 
         {
             AskQuestion(question);
@@ -31,6 +42,10 @@ static class Validator
                 DisplayErrorText($"Sorry I could not understand you. I don't see any text. Please enter at least {MIN_RESPONSE_LENGTH} characters");
             }//while//
         }//AskQuestionString//
+
+        /*
+         * Ask for and validate the state code.
+         */
         public static string AskQuestionState(string question) 
         {
             AskQuestion(question);
@@ -42,6 +57,10 @@ static class Validator
                 DisplayErrorText("Sorry I did not get your State Code please use the standard state code abbreviations");
             }
         }//AskQuestionState//
+
+        /*
+         * Ask for and validate the zip code.
+         */
         public static int AskQuestionZipCode(string question)
         {
             AskQuestion(question);
@@ -56,6 +75,10 @@ static class Validator
                 DisplayErrorText($"Your Zip Code must be {ZIP_CODE_LENGTH} numeric digits - Please re-enter.");
             }
         }//AskQuestionZipCode//
+
+        /*
+         * Ask for and validate boolean input.
+         */
         public static bool AskQuestionBool(string question)
         {
             AskQuestion(question);
@@ -69,6 +92,10 @@ static class Validator
                 DisplayErrorText("Sorry I was unable to understand your response. Please use only Y(y) for yes or N(n) for no.");
             }//while//
         }//AskQuestionBool//
+
+        /*
+         * Helper method for asking a question.
+         */
         private static void AskQuestion(string question)
         {
             DisplayUserOutput(question);
@@ -79,25 +106,28 @@ static class Validator
             DisplayUserOutput("Press Enter to Exit");
             GetUserInput();
         }//EchoResponse//
+
+        /*
+         * Helper method to display error text.
+         */
         private static void DisplayErrorText(string errorText)
         {
             DisplayUserOutput(errorText);
         }//DisplayErrorText//
+
+        /*
+         * Helper method to get user input.
+         */
         private static string GetUserInput()
         {
             return Console.ReadLine();
         }//getUserInput//
+
+        /*
+         * Helper method to display user output.
+         */
         private static void DisplayUserOutput(string text)
         {
             Console.WriteLine(text);
         }//DisplayUserOutput//        
-
-
-
-
-
-
-
-
-
-}
+}//Validator//
